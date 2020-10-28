b4dde6542e520d608422f50c0d1761319274e58e
Emmanuel Zoolander <12421140+emmanuelcu@users.noreply.github.com>
2020-10-28 15:17:12 -0600

    Play Sound Function
    The function created for play the sound are included in the code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
+    
+//    Function to play sound in swift
+    
+    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else
+//        { return }
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode:.default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+////            The following line is required for the player to work on iOS. Change the file type accordingly
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+//
+//            guard let player = player else {
+//                return
+//            }
+//
+//            player.play()
+//
+//        }catch let error {
+//            print(error.localizedDescription)
+//        }
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 
 }
 
